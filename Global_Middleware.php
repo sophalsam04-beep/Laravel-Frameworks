5077b299acfb786d29c1e2580007a4ebd4595780
សំ សុផល <sophalsam04@gmail.com>
2026-08-16 20:59:35 +0700

    Create Global_Middleware.php

---
+++ Global_Middleware.php
@@ -0,0 +1,43 @@
+<?php
+
+        // Global Middleware and Logic Middleware
+    namespace App\Http\Middleware;
+
+    use Closure;
+    use Illuminate\Http\Request;
+        use Illuminate\Support\Facades\Log;
+        use Symfony\Component\HttpFoundation\Response;
+
+    class LogAllRequest{
+        public function handle(Request $request, Closure $next): Response
+        {
+            Log::info('Incoming Request', [
+                'method' => $request->method(),
+                'url' => $request->fullUrl(),
+                'ip' => $request->ip(),
+                'time' => now(),
+            ]);
+
+            // call request to middleware
+
+            $response = $next($request);
+
+            // after response created
+
+            Log::info('Response Status', [
+                'status' => $response->getStatusCode(),
+            ]);
+
+            return $response;
+        }
+    }
+
+
+
+
+
+
+
+
+
+?>
