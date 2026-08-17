d3d9818d28d0a11aaec6ebbeb9511f77f53dc5fd
សំ សុផល <sophalsam04@gmail.com>
2026-08-17 20:37:26 +0700

    Create read_write_database_configurations.php

---
+++ read_write_database_configurations.php
@@ -0,0 +1,25 @@
+<?php
+        // Read and Write Splittings
+    // read and write host into laravel
+
+    'mysql' => [
+        'read'=> [
+            'host'=> ['10.168.19.130'],
+        ],
+    'write'=>[
+        'host'=>['127.0.0.1'],
+    ],
+    'sticky'=>true ,  // can read after write connection
+    'driver'=> 'mysql',
+
+    // other option
+]
+
+
+
+
+
+
+
+
+?>
