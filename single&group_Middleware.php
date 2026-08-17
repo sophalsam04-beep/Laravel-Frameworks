69c838c0b9c7a4a2c1fb0123ee77ba5e23d06653
សំ សុផល <sophalsam04@gmail.com>
2026-08-17 16:27:56 +0700

    Create single&group_Middleware.php

---
+++ single&group_Middleware.php
@@ -0,0 +1,40 @@
+<?php
+    // Applying the Group to Route
+
+        // Route by Group class
+        // Route by Single class
+
+
+    use Illuminate\Http\Request;
+    use App\Http\Controllers\Admin\DashboardController;
+    use App\Http\Controllers\Admin\UserController;
+
+
+    // Applying to route group
+    Route::middleware(['admin'])->prefix('admin')->name('admin.')->group(function () {
+          Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+          Route::resource('users', UserController::class);
+    });
+
+        // Apply to Single Route
+    Route::get('/reports', [DashboardController::class, 'reports'])
+        ->middleware('admin')
+        ->name('reports');
+
+    Route::middleware(['auth'])->prefix('auth')->name('auth.')->group(function () {
+            // Route by group
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('client', [DashboardController::class, 'client']);
+    });
+
+    
+    
+    
+    // Single Route Middleware
+    Route::get('/detail', [DashboardController::class, 'detail'])
+        ->middleware('admin')
+        ->name('detail');
+
+
+
+?>
