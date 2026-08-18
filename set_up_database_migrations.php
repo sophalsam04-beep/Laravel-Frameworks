96153c93ed25cab7928cc7453936a5aff42da9e8
សំ សុផល <sophalsam04@gmail.com>
2026-08-18 16:26:56 +0700

    Create set_up_database_migrations.php

---
+++ set_up_database_migrations.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Set up Database -> Migration
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+
+        return new class extends Migration
+        {
+            public function up(): void
+            {
+                Schema::create('products', function (Blueprint $table) {
+                    $table->id();
+                    $table->string('name');
+                    $table->text('description');
+                    $table->decimal('price', 10,2);
+                    $table->integer('stock')->default(0);
+                    $table->string('category');
+                    $table->boolean('is_active')->default(true);
+                    $table->timestamps();
+
+                });
+            }
+
+            public function down(): void{
+                Schema::dropifexists('products');
+            }
+        }
+
+
+
+?>
